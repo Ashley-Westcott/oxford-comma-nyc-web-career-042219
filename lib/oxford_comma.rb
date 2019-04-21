0caf9cb9704090571ec3a4575183b13edb72f0a1
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-21 01:23:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    puts array.join
+    return array.join
     elsif array.length == 2
-      puts array.join(" and ")
+      return array.join(" and ")
     end
   end
 end
