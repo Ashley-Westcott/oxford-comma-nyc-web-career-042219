b5bb34d1ce76ddf3e4b0e955bcdd0857e190ed64
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-21 01:31:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length == 3
     return array[0]+ ", " + array[1] + ", and " + array[2]
+  else 
+    return array.length-1.join
   end
 end
